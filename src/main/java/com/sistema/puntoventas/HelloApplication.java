db0a34e88e007f23b27435d0416486160c0ae938
isaac gonzalez <isa.gonzalezl@duocuc.cl>
2026-04-23 20:33:01 -0400

    integración panel principal

--- src/main/java/com/sistema/puntoventas/HelloApplication.java
+++ src/main/java/com/sistema/puntoventas/HelloApplication.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/sistema/puntoventas/PanelProductos-vista.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/sistema/puntoventas/panelPrincipalVista.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Cafeteria Eluney");
         stage.setScene(scene);
