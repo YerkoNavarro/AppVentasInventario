e3e4c51f8e60cd4a1f522c764cd798f31e8bac13
isaac gonzalez <isa.gonzalezl@duocuc.cl>
2026-05-14 18:11:54 -0400

    mejora interfaz grafica

--- src/main/java/com/sistema/puntoventas/HelloApplication.java
+++ src/main/java/com/sistema/puntoventas/HelloApplication.java
@@ -14,6 +14,7 @@ import com.sistema.puntoventas.service.VentaService;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -24,11 +25,33 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/sistema/puntoventas/panelPrincipalVista.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Eluney");
+        stage.setWidth(1920);
+        stage.setHeight(1080);
         stage.setScene(scene);
         stage.setResizable(true);
         stage.setMaximized(true);
+        
+        // Listener para controlar la posición cuando se restaura la ventana
+        stage.maximizedProperty().addListener((obs, wasMaximized, isMaximized) -> {
+            if (!isMaximized) {
+                // Al restaurar, centrar la ventana en la pantalla
+                double screenWidth = javafx.stage.Screen.getPrimary().getBounds().getWidth();
+                double screenHeight = javafx.stage.Screen.getPrimary().getBounds().getHeight();
+                
+                double x = (screenWidth - stage.getWidth()) / 2;
+                double y = (screenHeight - stage.getHeight()) / 2;
+                
+                // Asegurar que la ventana no quede fuera de pantalla
+                if (y < 0) y = 20;
+                if (x < 0) x = 20;
+                
+                stage.setX(x);
+                stage.setY(y);
+            }
+        });
+        
         stage.show();
 
         DbManager dbManager = new DbManager();
